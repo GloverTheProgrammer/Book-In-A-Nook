e3ea33575f4273b84cf8e073d4fbf6a1f670d81c
David <davidjrglover@gmail.com>
2024-04-24 02:52:21 -0400

    login and create user api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,25 @@
 require('dotenv').config();
 const connection = require("./db");
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const createUser = require("./routes/createUser");
+const authRoutes = require("./routes/login");
 
 
+// middleware
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Test nodemon!'));
+// routes
+app.use("/api/users", createUser);
+app.use("/api/login", authRoutes);
+
+// database connection 
+connection();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 
 
-connection();
+// test
+app.get('/', (req, res) => res.send('Server Running'));
