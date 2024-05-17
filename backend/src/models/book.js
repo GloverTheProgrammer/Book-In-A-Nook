1e4fe96700b533618591a095978c0c4babd1aa73
David <davidjrglover@gmail.com>
2024-05-17 15:08:14 -0400

    Comments

--- backend/src/models/book.js
+++ backend/src/models/book.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     book_title: { type: String, required: true },
     book_author: { type: String, required: true },
-    read: { type: Boolean, required: true }, // intialized as false
+    read: { type: Boolean, required: true }, // intialized as false in the backend
     google_books_id: { type: String, required: true}
 });
 
