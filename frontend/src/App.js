03fa6fff846b49d81a10d8b272a6e9da04ad7bfd
Benjamin Weeg <Benjaminweeg@gmail.com>
2024-05-24 14:53:35 -0400

    Goto library page on first load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 // App.js (javascript)
 // Benjamin, Sam, Vinh, David
 // Started: 
-// Last edited 2024-05-14 (yyyy mm dd)
+// Last edited 2024-05-24 (yyyy mm dd)
 
 import { Route, Routes, Navigate } from "react-router-dom";
-import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Library from "./components/Library";
@@ -15,12 +14,11 @@ function App() {
 
     return (
         <Routes>
-            {user && <Route path="/" exact element={<Main />} />}
             <Route path="/signup" exact element={<Signup />} />
             <Route path="/login" exact element={<Login />} />
             <Route path="/library/add-book" exact element={<AddBook />} />
             <Route path="/library" exact element={<Library />} />
-            <Route path="/" element={<Navigate replace to={user ? "/" : "/login"} />} />
+            <Route path="/" element={<Navigate replace to={user ? "/library" : "/login"} />} />
         </Routes>
     );
 }
